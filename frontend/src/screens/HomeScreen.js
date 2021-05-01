307eae198e9ca87310d2d045c03d419eae56325d
nadavschwartz <nadavschwartz58@gmail.com>
2021-05-01 09:35:25 -0700

    homescreen background image update

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,47 +3,47 @@ import React from "react";
 
 const HomeScreen = () => {
   return (
-    <div
-      className="intro"
-      style={{
-        backgroundImage: `url("https://mdbootstrap.com/img/Photos/Horizontal/Nature/full%20page/img%20%283%29.jpg")`,
-      }}
-    >
-      <div
-        className="mask"
-        style={{
-          background: "rgba(0, 0, 0, 0.5)",
-        }}
-      >
-        <MDBContainer
-          fluid
-          className="d-flex align-items-center justify-content-center h-100"
+    <>
+      <div className="intro">
+        <div
+          className="mask"
+          style={{
+            background: "rgba(0, 0, 0, 0.5)",
+          }}
         >
-          <MDBRow className="d-flex justify-content-center text-center">
-            <MDBCol>
-              <h2 className="display-2 font-weight-bold text-white pt-4 mb-2">
-                Hikeable
-              </h2>
+          <MDBContainer
+            fluid
+            className="d-flex align-items-center justify-content-center h-100"
+          >
+            <MDBRow className="d-flex justify-content-center text-center">
+              <MDBCol>
+                <h2 className="display-2 font-weight-bold text-white pt-4 mb-2">
+                  Hikeable
+                </h2>
 
-              <hr className="text-white" />
+                <hr className="text-white" />
 
-              <h4 className="text-white my-5">
-                OUTDOORS IS WHERE LIFE HAPPENS
-              </h4>
-              <MDBBtn
-                outline
-                href="#discover-more"
-                className="btn-outline-white "
-              >
-                Discover more
-                <i className="fa fa-book ms-2"></i>
-              </MDBBtn>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
+                <h4 className="text-white my-5">
+                  OUTDOORS IS WHERE LIFE HAPPENS
+                </h4>
+                <MDBBtn
+                  outline
+                  href="#discover-more"
+                  className="btn-outline-white "
+                >
+                  Discover more
+                  <i className="fa fa-book ms-2"></i>
+                </MDBBtn>
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+        </div>
       </div>
-      </div>
-      
+      <MDBContainer>
+        <p className="text-black">asda</p>
+        <MDBBtn>as</MDBBtn>
+      </MDBContainer>
+    </>
   );
 };
 
